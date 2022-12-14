fd784724a70140db3dab15e06e6d6d1e07d7dd38
SuzukiNaoto0422 <naojin.naojin@gmail.con>
2022-12-14 15:22:25 +0900

    MenuListクラスで使用するためのMenuクラスのインスタンスを作成

--- src/Menu.java
+++ src/Menu.java
@@ -1,6 +1,6 @@
 public class Menu {
     //リストに複数の要素（今回は文字列の商品名、数字の値段の２つの要素を持たせたい。）
-    //ここで作成したクラスをMenuListクラスで使用。
+    //ここで作成したクラスをMenuListクラスで使用
     private String name; //商品名
     private int price; //値段
 
