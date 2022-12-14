f9fb4c5c3385dc18e8c9eae4dd37519a349c71c8
SuzukiNaoto0422 <naojin.naojin@gmail.con>
2022-12-14 15:06:26 +0900

    2つのリストの作成

--- src/CafeMenu.java
+++ src/CafeMenu.java
@@ -4,22 +4,35 @@ import java.util.Arrays;
 public class CafeMenu {
     public static void main(String[] args) {
 
-        ArrayList<String> maneList = new ArrayList<>();
+        ArrayList<String> menuList = new ArrayList<>();
         // 順に要素を追加
-        maneList.add("ブラックコーヒー");
-        maneList.add("カプチーノ");
-        maneList.add("ホワイトモカ");
-        maneList.add("キャラメルラテ");
+        menuList.add("ブラックコーヒー");
+        menuList.add("カプチーノ");
+        menuList.add("ホワイトモカ");
+        menuList.add("キャラメルラテ");
 
         ArrayList<Integer> priceList = new ArrayList<>();
         //addAllを使用
         priceList.addAll(Arrays.asList(420,520,480,620));
 
-        for (int i = 0; i < maneList.size(); i++) {
-            String name = maneList.get(i);
+        //通常のfor文
+        for (int i = 0; i < menuList.size(); i++) {
+            String nameName = menuList.get(i);
             int price = priceList.get(i);
-            System.out.println(name + "の値段は" + price + "円です。");
+            System.out.println(nameName + "の値段は" + price + "円です。");
         }
+
+        System.out.println("\n");
+
+        //拡張for文
+        for (String menu : menuList) {
+            //引数menuが指定されたコーヒーのメニュー。menuList.indexOf(menu)は、その位置(0から始まるインデックス)を返します。priceList.get(...)は、指定された位置の要素を返す。
+            //つまり、priceList.get(menuList.indexOf(menu))は、menuに対応するコーヒーの価格を返し、この値がint priceに格納される。
+            //menuが"ブラックコーヒー"の場合、menuList.indexOf(menu)は0を返し、priceList.get(0)は420を返す。そのため、priceには420が格納される。
+            int price = priceList.get(menuList.indexOf(menu));
+            System.out.println(menu + " costs " + price + " yen.");
+        }
+
 
     }
 }
