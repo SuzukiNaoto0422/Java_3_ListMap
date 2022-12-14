93041a3c389659e17ec34f22c778f99a9bf6a533
SuzukiNaoto0422 <naojin.naojin@gmail.con>
2022-12-14 15:08:23 +0900

    Mapの作成

--- src/MenuMap.java
+++ src/MenuMap.java
@@ -3,6 +3,7 @@ import java.util.Map;
 
 public class MenuMap {
     public static void main(String[] args) {
+
         Map<String,Integer> cafeMenu = new HashMap<>();//格納順で取り出したい場合はLinkedHashMap、自然順序で取り出したい場合はTreeMap
         cafeMenu.put("ブラックコーヒー",420);//Mapにおいての要素の追加はaddではなくput
         cafeMenu.put("カプチーノ",520);
