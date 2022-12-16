31f5fdc1d0f5df5fd93771f7fbf75b02fad2c520
SuzukiNaoto0422 <naojin.naojin@gmail.con>
2022-12-16 15:21:26 +0900

    Menuクラスのインスタンスを使用し複数の要素を持つリストを作成

--- src/MenuList.java
+++ src/MenuList.java
@@ -13,14 +13,24 @@ public class MenuList {
         menus.addAll(Arrays.asList(new Menu("ホワイトモカ",480),
                                    new Menu("キャラメルラテ",620)));
         //通常のfor文
-        for (int i = 0; i< menus.size(); i++) {
-            System.out.println((menus.get(i).getName() + "の値段" + menus.get(i).getPrice() + "です。"));
+        for (int i = 0; i < menus.size(); i++) {
+            System.out.println((menus.get(i).getName() + "の値段" + menus.get(i).getPrice() + "円です。"));
         }
 
         System.out.println("\n");
         //拡張for文
         for (Menu menu : menus) {
-            System.out.println(menu.getName() + "の値段は" + menu.getPrice() + "です。");
+            System.out.println(menu.getName() + "の値段は" + menu.getPrice() + "円です。");
+        }
+
+        System.out.println("\n");
+        //try-catch文
+        try {
+            for (int i =  0 ; i <= menus.size(); i++) {
+                System.out.println((menus.get(i).getName() + "の値段は" + menus.get(i).getPrice() + "円です。"));
+            }
+        }catch (IndexOutOfBoundsException e) {//リストの範囲を超えた指示がされた際の例外クラス
+            System.out.println("リストの範囲を超えた指定です。");
         }
     }
 }
